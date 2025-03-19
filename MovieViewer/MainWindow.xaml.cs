175080021f2ee106d0d35e25c819619b224cb11d
stanoja1234 <stanoja.bozinov@student.um.si>
2025-03-19 19:57:20 +0100

    Added MovieClass

--- MovieViewer/MainWindow.xaml.cs
+++ MovieViewer/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
 
 namespace MovieViewer
 {
@@ -7,9 +9,70 @@ namespace MovieViewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Movie> Movies { get; set; }
+        public Movie? SelectedMovie { get; set; }
         public MainWindow()
         {
             InitializeComponent();
-        }
+            Movies = new ObservableCollection<Movie>
+            {
+                new Movie
+    {
+        Name = "Inception",
+        Director = "Christopher Nolan",
+        ReleaseYear = "2010",
+        Description = "A thief who enters the dreams of others to steal secrets from their subconscious.",
+        Actors = new ObservableCollection<string> { "Leonardo DiCaprio", "Joseph Gordon-Levitt", "Elliot Page" },
+        Genres = new ObservableCollection<string> { "Sci-Fi", "Thriller" },
+        ImagePath = "C:\\Users\\STANOJA\\Desktop\\faks2sem3god\\MovieViewer\\MovieViewer\\inception.jpg",
+        IsFavorite = false
+    },
+
+    new Movie
+    {
+        Name = "Interstellar",
+        Director = "Christopher Nolan",
+        ReleaseYear = "2014",
+        Description = "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+        Actors = new ObservableCollection<string> { "Matthew McConaughey", "Anne Hathaway", "Jessica Chastain" },
+        Genres = new ObservableCollection<string> { "Sci-Fi", "Adventure", "Drama" },
+        ImagePath = "C:\\Users\\STANOJA\\Desktop\\faks2sem3god\\MovieViewer\\MovieViewer\\inception.jpg",
+        IsFavorite = true
+    },
+
+    new Movie
+    {
+        Name = "The Dark Knight",
+        Director = "Christopher Nolan",
+        ReleaseYear = "2008",
+        Description = "Batman sets out to dismantle the remaining criminal organizations in Gotham with the help of allies.",
+        Actors = new ObservableCollection<string> { "Christian Bale", "Heath Ledger", "Aaron Eckhart" },
+        Genres = new ObservableCollection<string> { "Action", "Crime", "Drama" },
+        ImagePath = "C:\\Users\\STANOJA\\Desktop\\faks2sem3god\\MovieViewer\\MovieViewer\\inception.jpg",
+        IsFavorite = false
     }
+            };
+            DataContext = this;
+        }
+
+        private void MovieListView_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (MovieListView.SelectedItem is Movie selectedMovie)
+            {
+                MessageBox.Show($"Selected Movie: {selectedMovie.Name}", "Movie Info");
+            }
+        }
+
+        private void AddToFavoritesButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MovieListView.SelectedItem is Movie selectedMovie)
+            {
+                selectedMovie.IsFavorite = !selectedMovie.IsFavorite;  
+            }
+        }
+
+
+
+    }
+
 }
---
+++ MovieViewer/Movie.cs
@@ -0,0 +1,43 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieViewer
+{
+  public  class Movie : INotifyPropertyChanged
+    {
+        private bool isFavorite;
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Director { get; set; }
+        public string ReleaseYear { get; set; }
+        public ObservableCollection<string> Actors { get; set; }
+        public ObservableCollection<string> Genres { get; set; }
+        public string ImagePath { get; set; }
+
+        public bool IsFavorite
+        {
+            get => isFavorite;
+            set
+            {
+                if (isFavorite != value)
+                {
+                    isFavorite = value;
+                    OnPropertyChanged(nameof(IsFavorite));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
